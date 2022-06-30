1da492d34f48ef3808dcfc1cd23efc23d6f9e665
Anton Belmisov <antonbelmisov@mail.ru>
2022-06-30 19:50:06 +0300

    We wrote the logic and conducted testing, found out that you can go into the negative on the map.

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,87 @@
+package ru.netology.web.test;
+
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.assertEquals;
+
+public class MoneyTransferTest {
+
+
+    @BeforeEach
+    public void openingPage() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    //Перевод с 1ой карты на 2ю
+    void transferBetweenCards1() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+        var firstCardInfo = DataHelper.getFirstCardInfo();
+        var secondCardInfo = DataHelper.getSecondCardInfo();
+        int amount = 5000;
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo) - amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo) + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+
+    }
+
+    @Test
+    //Перевод суммы больше лимита
+    void transferringAnAmountOverTheLimit() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+        var firstCardInfo = DataHelper.getFirstCardInfo();
+        var secondCardInfo = DataHelper.getSecondCardInfo();
+        int amount = 15000;
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo) - amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo) + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+    @Test
+    //Пробуем перевести ноль
+    void tryingToTranslateZero() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+        var firstCardInfo = DataHelper.getFirstCardInfo();
+        var secondCardInfo = DataHelper.getSecondCardInfo();
+        int amount = 0;
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo) - amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo) + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+
+}
+
